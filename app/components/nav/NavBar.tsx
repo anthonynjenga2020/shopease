6e965e3a4333df0c06cd758022b585f29b7a4106
anthonynjenga254 <anthonynjenga2020@gmail.com>
2024-06-03 21:29:45 +0300

    setting up components

---
+++ app/components/nav/NavBar.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Container from "../Container";
+
+const NavBar = () => {
+    return <div className="
+    sticky
+    top-0
+    w-full
+    bg-slate-200
+    z-30
+    shadow-sm">
+        <div className="py-4 border-b-[1px]">
+            <Container>
+                <div>
+                    <Link href="/">Shopease</Link>
+                    <div>Search</div>
+                    <div>
+                        <div>CartCount</div>
+                        <div>UserMenu</div>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    </div>;
+}
+ 
+export default NavBar;
